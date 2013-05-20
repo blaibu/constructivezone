5a35d3a6d396af94ac2eedc68e6f2b5adee1abb4
Barry Lai <blai@getabine.com>
2013-05-20 16:28:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Constructivezone::Application.config.secret_token = '5e8331e456f2ec0d52c0e4d9a3e1609fc296c7ecfceadacbfd0b1d19b684b4363e63bdfda5dc9a5943d06158a17fd6fe8ac003e98e5f57b73240ec78098bf260'
